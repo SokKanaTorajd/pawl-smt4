01a7bc0d5b8adf4c952c55df3d727ded09dda73f
Wijatama <44823545+SokKanaTorajd@users.noreply.github.com>
2020-02-27 09:34:54 +0700

    Update app.py

--- Review-Flask/app.py
+++ Review-Flask/app.py
@@ -1,9 +1,39 @@
-# file: home.py
+# file: app.py
 # contoh halaman web dengan Flask
 
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return "<h2>Halo</h2><p>Selamat datang di kelas Pengembangan Aplikasi Web dengan Flask</p>"
+
+# @app.route('/profil')
+# def profil():
+#     return "<h2>Profil</h2><p>Aplikasi ini dibuat oleh <strong>Wijatama D</strong></p>"
+
+# @app.route('/salam/<nama>')
+# def salam(nama):
+#     return "<h2>Selamat Datang!</h2><p>Selamat datang <strong>"+str(nama)+"</strong> di Aplikasi Flask</p>"
+
+# @app.route('/umur/<int:usia>')
+# def umur(usia):
+#     return "<h2>Umur</h2><p>umurku anda adalah <strong>"+str(usia)+" tahun</strong></p>"
+
+# @app.route('/pajak/<int:dana>')
+# def pajak(dana):
+#     hasil = (5/100) * dana
+#     return "<h2>Hitung Pajak</h2><p>Pajak Penghasilan (PPh) dari Rp <strong>"+str(dana)+"</strong> adalah <strong>"+str(hasil)+"</strong></p>"
+
+# @app.route('/semester/<periode>')
+# def semester(periode):
+#     if periode == "ganjil":
+#         return "<h2>Semester Ganjil</h2><p>Kuliah dilaksanakan pada bulan September-Desember. UAS dilaksanakan pada bulan Januari</p>"
+#     elif periode =="genap":
+#         return "<h2>Semester Genap</h2><p>Kuliah dilaksanakan pada bulan Februari-Mei. UAS dilaksanakan pada bulan Juni</p>"
+#     else:
+#         return "<h2>Pilih Semester</h2><p>Anda belum memilih semester ('ganjil' atau 'genap')</p>"
 
 @app.route('/')
 def home():
